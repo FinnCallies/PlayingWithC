ccdd34a40077d6ec2a02b2db7a123e891be717dc
Finn Callies <Finn.Callies@ibm.com>
2022-11-07 13:32:33 +0100

    [CRYPT] Bug in is_prime()

--- crypt/rsa.c
+++ crypt/rsa.c
@@ -6,8 +6,8 @@
 
 bool is_prime(double v) 
 {
-    for (int i = 2; i < sqrt(v); i++) {
-        if (fmod(v, i) != 0)
+    for (int i = 2; i <= sqrt(v); i++) {
+        if (fmod(v, (double) i) != 0)
             return false;
     }
     return true;
@@ -15,14 +15,15 @@ bool is_prime(double v)
 
 double get_primes(double least_significant, double most_significant, double *ret) 
 {
-    double *list = (double *) calloc(most_significant/log(most_significant), sizeof(double));
+    double *list = (double *) calloc(most_significant/log2(most_significant), sizeof(double));
     int index = 0;
     if (least_significant < 2) least_significant = 2;
     
+    printf("listing primes between %lf and %lf:\n", least_significant, most_significant);
     for (double i = least_significant; i < most_significant; i++) {
-        if (is_prime(most_significant))
-        {
-            list[index] = most_significant;
+        if (is_prime(i)) {
+            printf("found prime: %lf\n", i);
+            list[index] = i;
             index++;
         }
     }
@@ -72,12 +73,18 @@ int get_coprimes(int *prime_list, int len, int v, int *coprime_list)
     return index;
 }
 
-void gen_p_q(double *p, double *q)
+void gen_p_q(double *p, double *q, double l, double m)
 {
-    double *primes = (double *) calloc(((pow(2, 32))/log(pow(2, 32))), sizeof(double));
-    int prime_cnt = get_primes(pow(2, 8), pow(2, 32), primes);
+    printf("log2(%lf) = %lf\n", m, log2(m));
+    double *primes = (double *) calloc((m/log2(m)), sizeof(double));
+    printf("primes init\n");
+    int prime_cnt = get_primes(log2(m), m, primes);
+    printf("primes calculated\n");
+    
     *p = primes[rand() % prime_cnt];
-    printf("p = %d\n", *p);
+    int r = rand();
+    printf("r = %d; cnt = %d\n", r, prime_cnt);
+    printf("p = %lf\n", *p);
     do {
         *q = primes[rand() % prime_cnt];
     } while (!is_coprime(*p, *q));
@@ -85,13 +92,16 @@ void gen_p_q(double *p, double *q)
 
 void generate_rsa_key_pair()
 {
-    int e = pow(2, 16) + 1;
-    printf("e = %d\n", e);
+    double l, m = 0;
+    l = pow(2, 4); // 2^16
+    m = pow(2, 8); // 2^32
+    int e = l + 1;
+    printf("e = %d, m = %lf, l = %lf\n", e, m, l);
     double p, q = 0;
-    gen_p_q(&p, &q);
-    printf("q = %d\n", q);
-    int n = p * q;
-    int phi = (p - 1) * (q - 1);
+    gen_p_q(&p, &q, l, m);
+    printf("q = %lf\n", q);
+    double n = p * q;
+    double phi = (p - 1) * (q - 1);
 
-    printf("n = %d, phi(n) = %d\n", n, phi);
+    printf("n = %lf, phi(n) = %lf\n", n, phi);
 }
--- crypt/rsa.h
+++ crypt/rsa.h
@@ -9,7 +9,7 @@ double gcd(double a, double b);
 double get_prndm_prime(double least_significant, double most_significant);
 bool is_coprime(double a, double b);
 int get_coprimes(int *prime_list, int len, int v, int *coprime_list);
-void gen_p_q(double *p, double *q);
+void gen_p_q(double *p, double *q, double l, double m);
 void generate_rsa_key_pair();
 
 #endif
